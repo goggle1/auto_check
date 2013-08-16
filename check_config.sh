7694b65fe49a2e93ff16fbc737cee5ab37fe6cdd
goggle1 <goggle1@163.com>
2013-08-16 11:15:58 +0800

    1st commit

---
+++ check_config.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+MS_LIST="./mobile_key_password.list"
+
+function ms_get_config()
+{
+	LOGIN_IP=$1
+	LOGIN_PORT=5044
+	LOGIN_KEY=$2
+	LOGIN_PASS=$3
+	
+	KEY_FILE="secrets/$LOGIN_KEY"
+	./expect.sh $LOGIN_IP $LOGIN_PORT $KEY_FILE "${LOGIN_PASS}"
+}
+
+function ms_check_config()
+{
+	MS_IP=$1
+	MS_PORT=$2
+	#./check.sh $MS_IP $MS_PORT
+}
+
+function do_line()
+{
+	LINE=$1
+	PLATFORM=`echo $LINE|awk -F"," '{print $13}'`
+	if [ x"${PLATFORM}" = x"mobile" ];
+	then
+		MS_IP=`echo $LINE|awk -F"," '{print $3}'`
+		MS_PORT=`echo $LINE|awk -F"," '{print $4}'`
+		MS_IP2=`echo $LINE|awk -F"," '{print $5}'`
+		MS_PORT2=`echo $LINE|awk -F"," '{print $6}'`
+		KEY=`echo $LINE|awk -F"," '{print $14}'`
+		PASSWORD=`echo $LINE|awk -F"," '{print $15}'`
+		ms_get_config $MS_IP2 $KEY $PASSWORD
+		ms_check_config $MS_IP2 $MS_PORT
+	fi
+}
+
+while read LINE
+do
+	do_line ${LINE}
+done < ${MS_LIST}
---
+++ expect.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/expect -d
+
+set MS_IP   [lindex $argv 0 ]
+set MS_PORT [lindex $argv 1 ]
+set KEY1    [lindex $argv 2 ]
+set PASS1   [lindex $argv 3 ]
+
+set MS_LOG "/home/ms_check_config.log"
+
+set timeout 60
+
+spawn ssh -i $KEY1 -p $MS_PORT admin@$MS_IP
+
+expect { 
+	"(yes/no)? " { send "yes\r" }
+	"Last login:*" { }
+	timeout { exit 1 }
+       }
+
+set timeout 10
+
+expect "]$ "
+send "su -\r"
+expect "Password: " 
+send "$PASS1\r" 
+
+expect {
+	 "incorrect password*" { send "exit\r"; expect eof; exit 1 }
+	 "]# " {}
+       }
+
+send "ps -ef|grep mediaserver > $MS_LOG\r"
+expect "]# "
+
+send "netstat -tlnp >> $MS_LOG\r"
+expect "]# "
+
+send "iptables -L -n >> $MS_LOG\r"
+expect "]# "
+
+send "cat /proc/`/sbin/pidof mediaserver`/limits|grep \"open files\" >> $MS_LOG\r"
+expect "]# "
+
+send "cat /home/mediaserver/etc/ms.conf | grep load_hvod_module >> $MS_LOG\r"
+expect "]# "
+
+send "cat /home/mediaserver/etc/ms.conf | grep speed_peer_upload_limit >> $MS_LOG\r"
+expect "]# "
+
+send "cat /home/mediaserver/etc/ms.conf | grep -E \"hvod_peer_max_speed|hvod_dld_max_speed|hvod_mp4head_max_speed|hvod_speed_fresh_interval|hvod_max_pending_package|hvod_free_speed_pos\" >> $MS_LOG\r"
+expect "]# "
+
+send "cat /home/mediaserver/etc/ms.conf | grep accepter_thread_num >> $MS_LOG\r"
+expect "]# "
+
+#send "MEDIA1=\`cat /home/mediaserver/etc/ms.conf | grep service_devices | grep -v service_devices_reload_interval | awk -F\"=\" \'{print $2}\' | awk -F\" \" \'{print $1}\'\`; ls \"$MEDIA1\" | head -n 5 >> $MS_LOG\r"
+#expect "]#"
+send "ls /media1 | head -n 5 >> $MS_LOG\r"
+expect "]# "
+
+send "chown admin:admin $MS_LOG\r"
+expect "]# "
+
+send "exit\r"
+expect "]$ "
+
+send "exit\r"
+expect eof
+
+set timeout 60
+spawn scp -i $KEY1 -P $MS_PORT admin@$MS_IP:$MS_LOG $MS_IP.log
+expect "\r\n"
+expect eof
+
