d8ee6f34b4885c9b58ff1e8f9481b4e7cd67b5b0
goggle1 <goggle1@163.com>
2013-08-19 14:34:11 +0800

    main.sh main2.sh main3.sh

--- check.sh
+++ check.sh
@@ -3,6 +3,26 @@
 MS_IP=$1
 MS_PORT=$2
 
+function check_download()
+{
+	FILE="${MS_IP}.log"
+	DAT_LIST=`cat ${FILE} | grep "^/media"`
+	for DAT in $DAT_LIST
+	do
+		#echo "$DAT"
+		DAT_FILE=`echo $DAT|awk -F"/" '{print $3}'`
+		DAT_NAME=`echo $DAT_FILE|awk -F"." '{print $1}'`
+		#curl -v -r 0-499 http://${MS_IP}:${MS_PORT}/${DAT_NAME}.mp4 -o ${MS_IP}_${MS_PORT}_${DAT_NAME}.mp4
+		DOWNLOAD_FILE="${MS_IP}_${MS_PORT}_${DAT_NAME}.mp4"
+		if [ -f $DOWNLOAD_FILE ];
+		then
+			echo "ok_download $MS_IP $MS_PORT $DAT"
+		else
+			echo "fail_download $MS_IP $MS_PORT $DAT"
+		fi
+	done
+}
+
 function do_download()
 {
 	FILE="${MS_IP}.log"
@@ -22,7 +42,7 @@ function check_log()
 	if [ -f $FILE ]; 
 	then
 		echo "ok ${MS_IP}"
-		do_download ${MS_IP} ${MS_PORT}
+		#do_download ${MS_IP} ${MS_PORT}
 	else
 		echo "fail ${MS_IP}"
 	fi
